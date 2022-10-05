bfe7183a517e3688142805e0106105630bd2c26f
Vasudev Gupta <7vasudevgupta@gmail.com>
2022-10-05 09:33:45 +0000

    try execpt

--- scripts/extract_text_from_books.py
+++ scripts/extract_text_from_books.py
@@ -9,24 +9,31 @@ from tqdm.auto import tqdm
 
 target_filename = "books.jsonl"
 books_dir = Path("books")
-print(list(books_dir.iterdir()))
+print("total no of books:", len(list(books_dir.iterdir())))
 
 
-def extract_text_from_pdf(filename: str) -> List[str]:
+def extract_text_from_pdf(filename: str, verbose=True) -> List[str]:
     obj = open(filename, "rb")
     pdf = PyPDF2.PdfFileReader(obj)
     pages = []
-    for i in tqdm(range(pdf.numPages)):
-        pages.append(pdf.getPage(i).extractText())
+    iterator = tqdm(range(pdf.numPages)) if verbose else range(pdf.numPages)
+    for i in iterator:
+        page = pdf.getPage(i).extractText()
+        if page == "":
+            continue
+        pages.append(page)
     obj.close()
     return pages
 
 
 with jsonlines.open(target_filename, "a") as writer:
-    for filename in books_dir.iterdir():
-        content = {"pages": extract_text_from_pdf(filename)}
-        writer.write(content)
-
+    for filename in tqdm(list(books_dir.iterdir())):
+        try:
+            content = {"pages": extract_text_from_pdf(filename, verbose=False)}
+            writer.write(content)
+        except Exception as e:
+            print("skipping:", filename)
+            print(e)
 
 # just checking if we are able to load this data properly
 from datasets import load_dataset
