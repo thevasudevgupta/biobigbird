01cae908ecbf6b4160b72dbe156f6abe5cf09b6c
Vasudev Gupta <7vasudevgupta@gmail.com>
2023-03-13 02:20:03 +0530

    save progress

--- scripts/cool.py
+++ scripts/cool.py
@@ -1,8 +1,9 @@
-
 def build_datasets():
-    pubmed_ds = load_dataset("pubmed", streaming=True, split='train')
-    remove_columns = ['MedlineCitation', 'PubmedData']
-    pubmed_ds = pubmed_ds.map(lambda x: extract_abstracts(x), remove_columns=remove_columns)
+    pubmed_ds = load_dataset("pubmed", streaming=True, split="train")
+    remove_columns = ["MedlineCitation", "PubmedData"]
+    pubmed_ds = pubmed_ds.map(
+        lambda x: extract_abstracts(x), remove_columns=remove_columns
+    )
     print(pubmed_ds)
 
     ok = iter(pubmed_ds)
@@ -10,7 +11,7 @@ def build_datasets():
         print(next(ok))
         print("\n\n")
 
-    mimic3_ds = load_dataset('ddp-iitm/mimic3_raw_v2', streaming=True, split='train')
+    mimic3_ds = load_dataset("ddp-iitm/mimic3_raw_v2", streaming=True, split="train")
     print(mimic3_ds)
 
     ok = iter(mimic3_ds)
