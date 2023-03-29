98c8b03d231fb5a02bb7f6d6974528e6f1f276f7
Vasudev Gupta <7vasudevgupta@gmail.com>
2023-03-29 00:08:26 +0000

    final pretraining on pubmed abstracts only

--- scripts/cool.py
+++ scripts/cool.py
@@ -9,20 +9,20 @@ def build_datasets():
         print(next(ok))
         print("\n\n")
 
-    mimic3_ds = load_dataset("ddp-iitm/mimic3_raw_v2", streaming=True, split="train")
-    print(mimic3_ds)
+    # mimic3_ds = load_dataset("ddp-iitm/mimic3_raw_v2", streaming=True, split="train")
+    # print(mimic3_ds)
 
-    ok = iter(mimic3_ds)
-    for _ in range(2):
-        print(next(ok))
-        print("\n\n")
+    # ok = iter(mimic3_ds)
+    # for _ in range(2):
+    #     print(next(ok))
+    #    print("\n\n")
 
-    ds = interleave_datasets([pubmed_ds, mimic3_ds], stopping_strategy="all_exhausted")
-    print(ds)
+    # ds = interleave_datasets([pubmed_ds, mimic3_ds], stopping_strategy="all_exhausted")
+    # print(ds)
 
-    ok = iter(ds)
-    for _ in range(10):
-        print(next(ok))
-        print("\n\n")
+    # ok = iter(ds)
+    # for _ in range(10):
+    #     print(next(ok))
+    #     print("\n\n")
 
-    return ds
+    return pubmed_ds
